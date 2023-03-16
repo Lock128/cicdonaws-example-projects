d28874dfef712fc1a39cee363b33b01a0973ab66
Johannes Koch <johanneskoch@fico.com>
2023-03-16 01:12:06 +0100

    #9 initial cdk (not finished)

--- .projenrc.js
+++ .projenrc.js
@@ -8,5 +8,6 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
+
 });
 project.synth();
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,50 @@
-import { App, Stack, StackProps } from 'aws-cdk-lib';
+import { App, CfnOutput, Stack, StackProps, Tags } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { APIDataService } from './api/construct';
 
-export class MyStack extends Stack {
+
+export class CICDOnAWSStack extends Stack {
+  private readonly apiDataService: APIDataService;
+
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
+    this.apiDataService = new APIDataService(this, 'apidata-service');
+
     // define resources here...
+    this.applyTagging();
+    this.output();
+  }
+
+  private applyTagging() {
+    Tags.of(this).add('application', 'cicdonaws');
+    Tags.of(this).add('project', 'cicdonaws');
+  }
+
+  private output() {
+    new CfnOutput(this, 'apiDataService.apiURl', {
+      value: this.apiDataService.api.graphqlUrl,
+    });
+    new CfnOutput(this, 'apiDataService.apiKey', {
+      value: this.apiDataService.api.apiKey
+        ? this.apiDataService.api.apiKey
+        : '',
+    });
   }
 }
-
 // for development, use account/region from cdk cli
-const devEnv = {
+/*const devEnv = {
   account: process.env.CDK_DEFAULT_ACCOUNT,
   region: process.env.CDK_DEFAULT_REGION,
+};*/
+const devEnv = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: 'eu-central-1',
 };
 
 const app = new App();
 
-new MyStack(app, 'cicdonaws-example-projects-dev', { env: devEnv });
-// new MyStack(app, 'cicdonaws-example-projects-prod', { env: prodEnv });
+new CICDOnAWSStack(app, 'cicdonaws-example-projects-dev', { env: devEnv });
+// new CICDOnAWSStack(app, 'cicdonaws-example-projects-prod', { env: prodEnv });
 
 app.synth();
