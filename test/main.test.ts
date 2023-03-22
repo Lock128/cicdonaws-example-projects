acd6240e23cb06fa979aca9c7de5b20803fccc6d
Johannes Koch <johanneskoch@fico.com>
2023-03-23 00:54:18 +0100

    #2 include codecatalyst workflow

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,10 +1,10 @@
 import { App } from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
-import { MyStack } from '../src/main';
+import { CICDOnAWSStack } from '../src/main';
 
 test('Snapshot', () => {
   const app = new App();
-  const stack = new MyStack(app, 'test');
+  const stack = new CICDOnAWSStack(app, 'test');
 
   const template = Template.fromStack(stack);
   expect(template.toJSON()).toMatchSnapshot();
