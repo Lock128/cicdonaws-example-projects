7614603726f9a0c590e090f5530bcf37dac64f90
Johannes Koch <johanneskoch@fico.com>
2023-03-23 00:28:34 +0100

    2 include codecatalyst workflow

--- .projenrc.js
+++ .projenrc.js
@@ -1,9 +1,14 @@
 const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: '2.1.0',
+  cdkVersion: '2.61.0',
   defaultReleaseBranch: 'main',
   name: 'cicdonaws-example-projects',
-
+  prettier: true,
+  github: false,
+  gitignore: ['outputs.json'],
+  deps: [
+    '@aws-sdk/client-dynamodb@3.264.0',
+  ] /* Runtime dependencies of this module. */,
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
